607b6c81e729092bfeba698c23912b2b049dfe6e
AnnaBrum <annahelenabrumark@gmail.com>
2023-09-05 14:55:07 +0200

    created routes for registration and dashboard

---
+++ src/app/dashboard/page.jsx
@@ -0,0 +1,11 @@
+import styles from './page.module.css'
+
+export default function Dashboard() {
+  return (
+    <main className={styles.main}>
+      <div className={styles.description}>
+        DASHBOARD
+      </div>
+    </main>
+  )
+}
---
+++ src/app/registration/page.jsx
@@ -0,0 +1,11 @@
+import styles from './page.module.css'
+
+export default function Registration() {
+  return (
+    <main className={styles.main}>
+      <div className={styles.description}>
+        REGISTRATION
+      </div>
+    </main>
+  )
+}
