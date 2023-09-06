1476a833612e38f47c4c2be6fe7016e89a7d2112
AnnaBrum <annahelenabrumark@gmail.com>
2023-09-06 10:42:27 +0200

    new install of supabase with auth config

---
+++ app/page.tsx
@@ -0,0 +1,42 @@
+import { createServerComponentClient } from '@supabase/auth-helpers-nextjs'
+import { cookies } from 'next/headers'
+import Link from 'next/link'
+import LogoutButton from '../components/LogoutButton'
+
+
+export const dynamic = 'force-dynamic'
+
+
+
+export default async function Index() {
+  const supabase = createServerComponentClient({ cookies })
+
+  const {
+    data: { user },
+  } = await supabase.auth.getUser()
+
+  return (
+    <div className="">
+      <nav className="">
+        <div className="">
+          <div />
+          <div>
+            {user ? (
+              <div className="">
+                Hey, {user.email}!
+                <LogoutButton />
+              </div>
+            ) : (
+              <Link
+                href="/login"
+                className="py-2 px-4 rounded-md no-underline bg-btn-background hover:bg-btn-background-hover"
+              >
+                Login
+              </Link>
+            )}
+          </div>
+        </div>
+      </nav>
+    </div>
+  )
+}
