b8afd38e7f0c63f486070b5b837d67fa1f07f519
jaken92 <jaaakeeen@gmail.com>
2023-09-11 20:40:45 +0200

    inserting values through route handler

--- app/dbserv/page.tsx
+++ app/dbserv/page.tsx
@@ -1,41 +1,53 @@
+// TODO: Duplicate or move this file outside the `_examples` folder to make it a route
+
 import { createServerComponentClient } from '@supabase/auth-helpers-nextjs';
 import { cookies } from 'next/headers';
-import { redirect } from 'next/navigation';
-import DbClient from '@/components/DbClient/DbClient';
-import { useEffect, useState } from 'react';
-// import Link from "next/link";
 
-// export const dynamic = 'force-dynamic';
-interface Props {
-  // Define the type for the insert function
-  insertData: () => Promise<void>;
-}
+export const dynamic = 'force-dynamic';
 
-export default async function DbServ() {
+export default async function ServerComponent() {
+  // Create a Supabase client configured to use cookies
   const supabase = createServerComponentClient({ cookies });
-  const {
-    data: { session },
-  } = await supabase.auth.getSession();
 
-  if (!session) {
-    // this is a protected route - only users who are signed in can view this route
-    redirect('/start');
-  }
+  // This assumes you have a `todos` table in Supabase. Check out
+  // the `Create Table and seed with data` section of the README 👇
+  // https://github.com/vercel/next.js/blob/canary/examples/with-supabase/README.md
+  const { data: users } = await supabase.from('users').select();
 
-  const {
-    data: { user },
-  } = await supabase.auth.getUser();
+  // console.log(JSON.stringify(users));
 
-  const userId = user?.id;
-
-  //inserting into users
-  async function myInsertFunction() {
-    // Your insert logic here
-  }
+  // const ParsedUsers = JSON.stringify(users);
 
   return (
-    <>
-      <DbClient insert={myInsertFunction}></DbClient>
-    </>
+    <div>
+      <h1>Intro and ID</h1>
+      <ol>
+        {users?.map((item) => (
+          <li key={item.id}>{item.intro}</li>
+        ))}
+      </ol>
+      <form
+        className="flex-1 flex flex-col w-full justify-center gap-2 text-foreground"
+        action="/route-handler"
+        method="post"
+      >
+        <label className="text-md" htmlFor="password">
+          Intro Text
+        </label>
+        <input
+          className="rounded-md px-4 py-2 bg-inherit border mb-6"
+          type="text"
+          name="text"
+          placeholder="intro text"
+          required
+        />
+        <button
+          formAction="/route-handler"
+          className="border border-gray-700 rounded px-4 py-2 text-black mb-2"
+        >
+          Insert Values
+        </button>
+      </form>
+    </div>
   );
 }
