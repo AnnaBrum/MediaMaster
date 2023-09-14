6564f8c31752c76e51aad839775cc8f67674459a
AnnaBrum <annahelenabrumark@gmail.com>
2023-09-14 13:24:05 +0200

    account clean up

--- app/account/page.tsx
+++ app/account/page.tsx
@@ -3,57 +3,57 @@ import { cookies } from "next/headers";
 import LogoutButton from "../../components/LogoutButton/LogoutButton";
 import { redirect } from "next/navigation";
 import { HamburgerMenu } from "@/components/HamburgerMenu/HamburgerMenu";
-import { UUID } from "crypto";
+
 
 export const dynamic = "force-dynamic";
 
-interface User {
-  id: number;
-  user_id: UUID;
-  firstname: string;
-  lastname: string;
-}
+// interface User {
+//   id: number;
+//   user_id: UUID;
+//   firstname: string;
+//   lastname: string;
+// }
 
 export default async function Account() {
   const supabase = createServerComponentClient({ cookies });
-  const {
-    data: { session },
-  } = await supabase.auth.getSession();
+  // const {
+  //   data: { session },
+  // } = await supabase.auth.getSession();
 
-  if (!session) {
-    // this is a protected route - only users who are signed in can view this route
-    redirect("/start");
-  }
+  // if (!session) {
+  //   // this is a protected route - only users who are signed in can view this route
+  //   redirect("/start");
+  // }
 
-  const userInfoResponse = await supabase.from("users").select();
-  let userInfo: User[] = [];
+//   const userInfoResponse = await supabase.from("users").select();
+//   let userInfo: User[] = [];
 
-  if (userInfoResponse && userInfoResponse.data) {
-    userInfo = userInfoResponse.data.map((user: any) => ({
-      id: user.id,
-      user_id: user.user_id,
-      firstname: user.firstname,
-      lastname: user.lastname,
-    }));
-  } else {
-    redirect("/login");
-  }
+//   if (userInfoResponse && userInfoResponse.data) {
+//     userInfo = userInfoResponse.data.map((user: any) => ({
+//       id: user.id,
+//       user_id: user.user_id,
+//       firstname: user.firstname,
+//       lastname: user.lastname,
+//     }));
+//   } else {
+//     redirect("/login");
+  
 
-  // const userInfo : User[] = await supabase.from("users").select();
+const { data: users } = await supabase.from("users").select()
+
+  console.log(users);
 
   return (
     <div className="profile py-8">
       <HamburgerMenu />
       <div className="py-20">
         <h1 className="text-xl font-bold">Välkommen till Media Watch!</h1>
-        {userInfo ? (
+        {users?.map((user) => (
           <div>
-            <p>Hey, {userInfo[0].firstname}!</p>
+            <p>Hey, {user.firstname}!</p>
             <LogoutButton />
           </div>
-        ) : (
-          redirect("/login")
-        )}
+        ))}
       </div>
     </div>
   );
