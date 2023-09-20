f6c6c38bc279b12ba4c7c0cfa7173fa0319ce41d
jaken92 <jaaakeeen@gmail.com>
2023-09-20 16:52:53 +0200

    add subscription searchfield work in progress
    started working on logic to present services from db upon user input

--- app/home/add-subscription/page.jsx
+++ app/home/add-subscription/page.jsx
@@ -3,15 +3,56 @@
 import { createClientComponentClient } from '@supabase/auth-helpers-nextjs';
 import { useEffect, useState } from 'react';
 
-// import '../styles/globals.css';
-
-
 export default function ClientComponent() {
   const supabase = createClientComponentClient();
+  const [subsData, setSubsData] = useState([]);
+  const [input, setInput] = useState('');
+  const [inputLength, setInputLength] = useState();
 
-  return(
+  useEffect(() => {
+    const getData = async () => {
+      const { data } = await supabase.from('services').select();
+      if (data) {
+        setSubsData(data);
+      }
+    };
+
+    getData();
+  }, [supabase, setSubsData]);
+
+  useEffect(() => {
+    console.log(subsData);
+  }, [subsData]);
+  const handleChange = (e) => {
+    setInput(e.target.value);
+    setInputLength(input.length);
+    console.log(inputLength);
+  };
+
+  const filteredData = subsData.filter((item) => {
+    const str = input.charAt(0).toUpperCase() + input.slice(1);
+
+    return item.service_name.includes(str);
+  });
+
+  return (
     <>
-    <h1>This is the add subscription page, the from below is not finished. </h1>
+      <input
+        type="text"
+        placeholder="service-name"
+        onChange={handleChange}
+        value={input}
+      />
+      {filteredData.map((item) => {
+        return (
+          <ul key={item.id}>
+            <li>{item.service_name}</li>
+          </ul>
+        );
+      })}
+      <h1>
+        This is the add subscription page, the from below is not finished.{' '}
+      </h1>
       <form
         className="flex-1 flex flex-col w-full justify-center gap-2 text-foreground"
         action="/route-handler/add-sub"
@@ -34,8 +75,6 @@ export default function ClientComponent() {
           Insert Values
         </button>
       </form>
-
     </>
-  )
-
+  );
 }
