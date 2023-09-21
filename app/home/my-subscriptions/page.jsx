2b14581b44a507121d20d3c23bc8b101f5e0d1b6
jaken92 <jaaakeeen@gmail.com>
2023-09-21 15:09:10 +0200

    Filter my-subscriptions
    filter based on click on category-button

--- app/home/my-subscriptions/page.jsx
+++ app/home/my-subscriptions/page.jsx
@@ -1,11 +1,11 @@
-"use client";
-import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
-import { useEffect, useState, useRef } from "react";
-import { BrandBox } from "@/components/BrandBox/BrandBox";
-import { AddPlanContainer } from "@/components/AddPlanContainer/AddPlanContainer";
-import { CategoryButton } from "@/components/CategoryButton/CategoryButton";
-import styles from "./my-subscriptions.module.css";
-export const dynamic = "force-dynamic";
+'use client';
+import { createClientComponentClient } from '@supabase/auth-helpers-nextjs';
+import { useEffect, useState, useRef } from 'react';
+import { BrandBox } from '@/components/BrandBox/BrandBox';
+import { AddPlanContainer } from '@/components/AddPlanContainer/AddPlanContainer';
+import { CategoryButton } from '@/components/CategoryButton/CategoryButton';
+import styles from './my-subscriptions.module.css';
+export const dynamic = 'force-dynamic';
 
 export default function ClientComponent() {
   const supabase = createClientComponentClient();
@@ -15,19 +15,22 @@ export default function ClientComponent() {
   const [totalCost, setTotalCost] = useState(0);
   const [subsCount, setSubsCount] = useState(0);
   const [isLoading, setIsLoading] = useState(true);
-
+  const [filter, setFilter] = useState(false);
   useEffect(() => {
     setIsLoading(true);
     const getData = async () => {
-      const { data : userSubsData } = await supabase.from("user_subscriptions").select(`
+      const { data: userSubsData } = await supabase.from('user_subscriptions')
+        .select(`
     id,
     billing_start_date,
     billing_date,
     subscriptions:subscription_id (plan_name, price, services:service_id (service_name, service_logo,
     service_categories:category_id(category, id))
     )
-  `)
-      const { data : categoryData } = await supabase.from("service_categories").select();
+  `);
+      const { data: categoryData } = await supabase
+        .from('service_categories')
+        .select();
 
       if (userSubsData && categoryData) {
         setSubsData(userSubsData);
@@ -49,21 +52,23 @@ export default function ClientComponent() {
     setSubsCount(amountOfsubs);
   }, [subsData]);
 
-  const filteredByCategory = subsData
-  .filter((item) =>
-  item.subscriptions.services.service_categories.id === filteredCategory) 
-
   const handleClick = (e) => {
     setFilteredCategory(e.target.value);
+    setFilter(true);
+    console.log(e.target.value);
   };
 
+  const filteredByCategory = subsData.filter(
+    (item) =>
+      item.subscriptions.services.service_categories.id == filteredCategory
+  );
 
   console.log(categoryData);
 
   return (
     <div className={styles.pageWrapper}>
       <section className={styles.sectionOne}>
-        < CategoryButton />
+        <CategoryButton />
         <h1 className={styles.headingOne}>Mina Prenumerationer</h1>
         <AddPlanContainer />
         <input
@@ -73,8 +78,13 @@ export default function ClientComponent() {
         />
       </section>
       <section className={styles.sectionTwo}>
-        {categoryData.map((item) => (          
-          <button className={styles.categoryButton} key={item.id} onClick={handleClick} value={item.id}>
+        {categoryData.map((item) => (
+          <button
+            className={styles.categoryButton}
+            key={item.id}
+            onClick={handleClick}
+            value={item.id}
+          >
             {item.category}
           </button>
         ))}
@@ -86,22 +96,39 @@ export default function ClientComponent() {
           <span className={styles.result}>{totalCost} kr</span>
         </div>
         <div className={styles.amount}>
-          <h3 className={styles.sectionThreeInfo}>Antal</h3> 
+          <h3 className={styles.sectionThreeInfo}>Antal</h3>
           <span className={styles.result}>{subsCount} st</span>
         </div>
       </section>
       <section className={styles.sectionFour}>
-        <ul>
-          {filteredByCategory.map((item) => (
-            <li key={item.id}>
-              <BrandBox
-                logoUrl={item.subscriptions.services.service_logo}
-                serviceName={item.subscriptions.services.service_name}
-                cost={item.subscriptions.price}
-              /><p>{item.subscriptions.services.service_categories.id}</p>
-            </li>
-          ))}
-        </ul>
+        {!filter && (
+          <ul>
+            {subsData.map((item) => (
+              <li key={item.id}>
+                <BrandBox
+                  logoUrl={item.subscriptions.services.service_logo}
+                  serviceName={item.subscriptions.services.service_name}
+                  cost={item.subscriptions.price}
+                />
+                <p>{item.subscriptions.services.service_categories.id}</p>
+              </li>
+            ))}
+          </ul>
+        )}
+        {filter && (
+          <ul>
+            {filteredByCategory.map((item) => (
+              <li key={item.id}>
+                <BrandBox
+                  logoUrl={item.subscriptions.services.service_logo}
+                  serviceName={item.subscriptions.services.service_name}
+                  cost={item.subscriptions.price}
+                />
+                <p>{item.subscriptions.services.service_categories.id}</p>
+              </li>
+            ))}
+          </ul>
+        )}
       </section>
     </div>
   );
