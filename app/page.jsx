addb861c3e03d2c79821f9e169ccc15af5fedc2a
jaken92 <jaaakeeen@gmail.com>
2023-09-25 21:22:35 +0200

    updated redirect in index page, remade middleware into js file

--- app/page.jsx
+++ app/page.jsx
@@ -1,24 +1,19 @@
-'use client';
-import { createClientComponentClient } from '@supabase/auth-helpers-nextjs';
+import { createServerComponentClient } from '@supabase/auth-helpers-nextjs';
 import { redirect } from 'next/navigation';
+import { cookies } from 'next/headers';
 
-export default async function Index() {
-  const supabase = createClientComponentClient();
+export const dynamic = 'force-dynamic';
+
+export default async function ServerComponent() {
+  // Create a Supabase client configured to use cookies
+  const supabase = createServerComponentClient({ cookies });
   const {
     data: { session },
   } = await supabase.auth.getSession();
 
-  // if (session) {
-  //   //this is a protected route - only users who are signed in can view this route
-  //   redirect('/');
-  // }
-  //  else {
-  //   // Redirect to /home after 5 seconds
-  //   setTimeout(() => {
-  //     console.log("hejsan")
-  //     redirect("/home");
-
-  //   }, 5000)};
+  if (!session) {
+    redirect('/startpage');
+  }
 
   return (
     <>
@@ -31,3 +26,36 @@ export default async function Index() {
     </>
   );
 }
+
+// 'use client';
+// import { createClientComponentClient } from '@supabase/auth-helpers-nextjs';
+// import { redirect } from 'next/navigation';
+
+// export default async function Index() {
+//   const supabase = createClientComponentClient();
+//   const {
+//     data: { session },
+//   } = await supabase.auth.getSession();
+
+//   // if (session) {
+//   //   //this is a protected route - only users who are signed in can view this route
+//   //   redirect('/');
+//   // } else {
+//   //   // Redirect to /home after 5 seconds
+//   //   setTimeout(() => {
+//   //     console.log('hejsan');
+//   //     redirect('/home');
+//   //   }, 5000);
+//   // }
+
+//   return (
+//     <>
+//       <div className="welcomePage">
+//         <h1>
+//           Välkommen till <br /> Media Watch!
+//         </h1>
+//         <div className="welcomeAnimation"></div>
+//       </div>
+//     </>
+//   );
+// }
