cbc16b7b57aa8b661a14c39f447e41ed41b8fbb8
jaken92 <jaaakeeen@gmail.com>
2023-09-25 12:38:09 +0200

    export const force-dynamic in layout

--- app/home/layout.jsx
+++ app/home/layout.jsx
@@ -1,4 +1,5 @@
-import { HamburgerMenu } from "@/components/HamburgerMenu/HamburgerMenu";
+import { HamburgerMenu } from '@/components/HamburgerMenu/HamburgerMenu';
+export const dynamic = 'force-dynamic';
 
 export default function AccountLayout({ children }) {
   return (
