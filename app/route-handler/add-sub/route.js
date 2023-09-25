771fda5c632ad9dd15f8af884c1e4c0ecf9ff848
jaken92 <jaaakeeen@gmail.com>
2023-09-25 10:29:59 +0200

    test deploy

--- app/route-handler/add-sub/route.js
+++ app/route-handler/add-sub/route.js
@@ -1,42 +1,42 @@
-// TODO: Duplicate or move this file outside the `_examples` folder to make it a route
+// // TODO: Duplicate or move this file outside the `_examples` folder to make it a route
 
-import { createRouteHandlerClient } from '@supabase/auth-helpers-nextjs';
-import { cookies } from 'next/headers';
-import { NextResponse } from 'next/server';
+// import { createRouteHandlerClient } from '@supabase/auth-helpers-nextjs';
+// import { cookies } from 'next/headers';
+// import { NextResponse } from 'next/server';
 
-export const dynamic = 'force-dynamic';
+// export const dynamic = 'force-dynamic';
 
-export async function POST(request) {
-  const requestUrl = new URL(request.url);
-  const formData = await request.formData();
-  const introtext = String(formData.get('text'));
-  const supabase = createRouteHandlerClient({ cookies });
+// export async function POST(request) {
+//   const requestUrl = new URL(request.url);
+//   const formData = await request.formData();
+//   const introtext = String(formData.get('text'));
+//   const supabase = createRouteHandlerClient({ cookies });
 
-  // This assumes you have a `todos` table in Supabase. Check out
-  // the `Create Table and seed with data` section of the README 👇
-  // https://github.com/vercel/next.js/blob/canary/examples/with-supabase/README.md
+//   // This assumes you have a `todos` table in Supabase. Check out
+//   // the `Create Table and seed with data` section of the README 👇
+//   // https://github.com/vercel/next.js/blob/canary/examples/with-supabase/README.md
 
-  const {
-    data: { user },
-  } = await supabase.auth.getUser();
+//   const {
+//     data: { user },
+//   } = await supabase.auth.getUser();
 
-  const userId = user?.id;
-  await supabase
-    .from('users')
-    .insert({ user_id: userId, firstname: introtext });
+//   const userId = user?.id;
+//   await supabase
+//     .from('users')
+//     .insert({ user_id: userId, firstname: introtext });
 
-  //   if (error) {
-  //     return NextResponse.redirect(
-  //       `${requestUrl.origin}/registration?error=Could not authenticate user`,
-  //       {
-  //         // a 301 status is required to redirect from a POST to a GET route
-  //         status: 301,
-  //       }
-  //     );
-  //   }
+//   //   if (error) {
+//   //     return NextResponse.redirect(
+//   //       `${requestUrl.origin}/registration?error=Could not authenticate user`,
+//   //       {
+//   //         // a 301 status is required to redirect from a POST to a GET route
+//   //         status: 301,
+//   //       }
+//   //     );
+//   //   }
 
-  return NextResponse.redirect(`${requestUrl.origin}/dbserv`, {
-    // a 301 status is required to redirect from a POST to a GET route
-    status: 301,
-  });
-}
+//   return NextResponse.redirect(`${requestUrl.origin}/dbserv`, {
+//     // a 301 status is required to redirect from a POST to a GET route
+//     status: 301,
+//   });
+// }
--- app/route-handler/see-sub/route.js
+++ app/route-handler/see-sub/route.js
@@ -1,21 +1,19 @@
+// import { createRouteHandlerClient } from '@supabase/auth-helpers-nextjs';
+// import { cookies } from 'next/headers';
+// import { NextResponse } from 'next/server';
 
+// export const dynamic = 'force-dynamic';
 
-import { createRouteHandlerClient } from '@supabase/auth-helpers-nextjs';
-import { cookies } from 'next/headers';
-import { NextResponse } from 'next/server';
+// export async function GET(request) {
+//   const requestUrl = new URL(request.url);
+//   const supabase = createRouteHandlerClient({ cookies });
 
-export const dynamic = 'force-dynamic';
+//   const { data: users } = await supabase.from('users').select();
 
-export async function GET(request) {
-  const requestUrl = new URL(request.url);
-  const supabase = createRouteHandlerClient({ cookies });
-
-  const { data: users } = await supabase.from('users').select();
-
-  return NextResponse.redirect(`${requestUrl.origin}/dbserv`, {
-    status: 301,
-    headers: {
-      'X-Data': JSON.stringify(users), // Pass the fetched data as a custom header
-    },
-  });
-}
+//   return NextResponse.redirect(`${requestUrl.origin}/dbserv`, {
+//     status: 301,
+//     headers: {
+//       'X-Data': JSON.stringify(users), // Pass the fetched data as a custom header
+//     },
+//   });
+// }
