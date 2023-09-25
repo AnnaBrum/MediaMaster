04ed967cc430f325d8cfe8931bd7f1441139b31c
jaken92 <jaaakeeen@gmail.com>
2023-09-25 10:24:52 +0200

    deploy test

--- app/route-handler/see-sub/route.js
+++ app/route-handler/see-sub/route.js
@@ -1,21 +1,21 @@
 
 
-// import { createRouteHandlerClient } from '@supabase/auth-helpers-nextjs';
-// import { cookies } from 'next/headers';
-// import { NextResponse } from 'next/server';
+import { createRouteHandlerClient } from '@supabase/auth-helpers-nextjs';
+import { cookies } from 'next/headers';
+import { NextResponse } from 'next/server';
 
-// export const dynamic = 'force-dynamic';
+export const dynamic = 'force-dynamic';
 
-// export async function GET(request) {
-//   const requestUrl = new URL(request.url);
-//   const supabase = createRouteHandlerClient({ cookies });
+export async function GET(request) {
+  const requestUrl = new URL(request.url);
+  const supabase = createRouteHandlerClient({ cookies });
 
-//   const { data: users } = await supabase.from('users').select();
+  const { data: users } = await supabase.from('users').select();
 
-//   return NextResponse.redirect(`${requestUrl.origin}/dbserv`, {
-//     status: 301,
-//     headers: {
-//       'X-Data': JSON.stringify(users), // Pass the fetched data as a custom header
-//     },
-//   });
-// }
+  return NextResponse.redirect(`${requestUrl.origin}/dbserv`, {
+    status: 301,
+    headers: {
+      'X-Data': JSON.stringify(users), // Pass the fetched data as a custom header
+    },
+  });
+}
