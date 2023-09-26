4acebae7379a2c37af5f3200b4524b10ca862699
AnnaBrum <annahelenabrumark@gmail.com>
2023-09-26 11:09:07 +0200

    import redirect

--- app/create-account/page.jsx
+++ app/create-account/page.jsx
@@ -2,6 +2,7 @@ import { createServerComponentClient } from '@supabase/auth-helpers-nextjs';
 import Messages from './messages';
 import Link from 'next/link';
 import { cookies } from 'next/headers';
+import { redirect } from 'next/navigation';
 
 export const dynamic = 'force-dynamic';
 
