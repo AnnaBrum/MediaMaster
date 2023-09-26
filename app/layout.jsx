ca5905f2ea44af6742fab7028e02d6d7b9d821fa
AnnaBrum <annahelenabrumark@gmail.com>
2023-09-26 11:23:55 +0200

    moved fints to globals.css

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,39 +1,8 @@
 import './globals.css';
-import localFont from 'next/font/local';
-
-const sequel_sans = localFont({
-  src: [
-    {
-      path: './fonts/SequelSans_BoldDisplay.woff2',
-      weight: '400',
-      style: 'normal',
-    },
-    {
-      path: './fonts/SequelSans_BoldHead.woff2',
-      weight: '400',
-      style: 'normal',
-    },
-    {
-      path: './fonts/SequelSans_LightDisp.woff2',
-      weight: '700',
-      style: 'normal',
-    },
-    {
-      path: './fonts/SequelSans_MediumHead.woff2',
-      weight: '700',
-      style: 'normal',
-    },
-    {
-      path: './fonts/SequelSans_SemiBoldDisp.woff2',
-      weight: '700',
-      style: 'normal',
-    },
-  ],
-});
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en" className={sequel_sans.className}>
+    <html lang="en" >
       <body>
         <main>{children}</main>
       </body>
