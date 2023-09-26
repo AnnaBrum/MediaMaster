72fc11e385feb5551fa533b0c6d78040dffdd4c8
AnnaBrum <annahelenabrumark@gmail.com>
2023-09-26 11:53:14 +0200

    reset-password page

---
+++ app/reset-password/page.jsx
@@ -0,0 +1,9 @@
+"use client";
+
+export default function ResetPassword() {
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-center py-2">
+      <h1>Återställ lösenord</h1>
+    </div>
+  );
+}
