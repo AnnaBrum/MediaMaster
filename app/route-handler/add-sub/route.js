7f0f3527ca2f04487d7a61fb9144908dcbd28b66
jaken92 <jaaakeeen@gmail.com>
2023-09-27 20:32:53 +0200

    removed some console.logs from route-handler/add-sub

--- app/route-handler/add-sub/route.js
+++ app/route-handler/add-sub/route.js
@@ -15,22 +15,11 @@ export async function POST(request) {
   const users = formData.get('users');
   const supabase = createRouteHandlerClient({ cookies });
 
-  console.log(subscriptionId);
-  console.log(billingStartDate);
-  console.log(serviceName);
-  console.log(users);
-
   //split up date to take out just the day of the month.
   const dateString = billingStartDate;
 
   const dateParts = dateString.split('/');
-  const day = parseInt(dateParts[1], 10); // Parse the day (e.g., "25" becomes 25)
-
-  console.log(day);
-
-  // This assumes you have a `todos` table in Supabase. Check out
-  // the `Create Table and seed with data` section of the README 👇
-  // https://github.com/vercel/next.js/blob/canary/examples/with-supabase/README.md
+  const day = parseInt(dateParts[1], 10);
 
   const {
     data: { user },
@@ -44,18 +33,7 @@ export async function POST(request) {
     billing_date: day,
   });
 
-  //   if (error) {
-  //     return NextResponse.redirect(
-  //       `${requestUrl.origin}/registration?error=Could not authenticate user`,
-  //       {
-  //         // a 301 status is required to redirect from a POST to a GET route
-  //         status: 301,
-  //       }
-  //     );
-  //   }
-
   return NextResponse.redirect(`${requestUrl.origin}/home/add-subscription`, {
-    // a 301 status is required to redirect from a POST to a GET route
     status: 301,
   });
 }
