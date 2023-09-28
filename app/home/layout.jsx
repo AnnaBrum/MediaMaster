fc75b170031fcc16f19e95b2e997ca8f5689efe9
AnnaBrum <annahelenabrumark@gmail.com>
2023-09-28 18:32:52 +0200

    added push notice animation

--- app/home/layout.jsx
+++ app/home/layout.jsx
@@ -1,11 +1,5 @@
-import { HamburgerMenu } from '@/components/HamburgerMenu/HamburgerMenu';
-export const dynamic = 'force-dynamic';
+export const dynamic = "force-dynamic";
 
 export default function AccountLayout({ children }) {
-  return (
-    <>
-      <HamburgerMenu />
-      {children}
-    </>
-  );
+  return <>{children}</>;
 }
