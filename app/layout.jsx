0b770a20af669d7101e4f2e42cfb65177c8475e1
AnnaBrum <annahelenabrumark@gmail.com>
2023-10-06 19:27:43 +0200

    one signal

--- app/layout.jsx
+++ app/layout.jsx
@@ -4,18 +4,6 @@ export default function RootLayout({ children }) {
 
   return (
     <html lang="en">
-      <head>
-        <title>OneSignal + Next.js</title>
-        <meta
-          name="description"
-          content="Integrating OneSignal with a Next.js app."
-        />
-        <link rel="icon" href="/favicon.ico" />
-        <script
-          src="https://cdn.onesignal.com/sdks/OneSignalSDK.js"
-          async=""
-        ></script>
-      </head>
       <body>
         <main>{children}</main>
       </body>
