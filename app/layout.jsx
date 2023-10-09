dcb9e2a0fa0d54e21ccaf2fe6397bc8f33d11418
AnnaBrum <annahelenabrumark@gmail.com>
2023-10-09 13:06:18 +0200

    Added head to layout for OneSignal

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,9 +1,20 @@
 import "./globals.css";
 
 export default function RootLayout({ children }) {
-
   return (
     <html lang="en">
+      <head>
+        <title>Media Watch</title>
+        <meta
+          name="description"
+          content="Integrating OneSignal with a Next.js app."
+        />
+        <link rel="icon" href="/favicon.ico" />
+        <script
+          src="https://cdn.onesignal.com/sdks/OneSignalSDK.js"
+          async=""
+        ></script>
+      </head>
       <body>
         <main>{children}</main>
       </body>
